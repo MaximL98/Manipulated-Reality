0dea46a89149d50160938b39cb1ea2a0042f5bad
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2024-05-29 12:15:52 +0300

    Renamed files, Code now trains a basic audio model

--- Audio/Pre_process_audio.py
+++ Audio/Pre_process_audio.py
@@ -1,5 +1,5 @@
 import os
-from AudioFunctions import function_collection
+# from AudioFunctions import function_collection
 
 # Used for bi-coherence analysis
 import scipy.signal as signal
@@ -15,7 +15,7 @@ import librosa
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-NUM_VALUES = 20
+NUM_VALUES = 1000
 
 # This function is used to calculate the bispectrum of the signals
 def compute_bispectrum(samples):
@@ -50,6 +50,7 @@ def calculate_amplitude_spectrum(signals):
 # Ideal average time is 5 seconds, the audio was sampled at 22050 Hz, so target length is 110250
 def trim_audio(audio, seconds, fs):
     target_length = seconds * fs
+    target_length = int(target_length)
     if len(audio) > target_length:
         audio = audio[:target_length]
     elif len(audio) < target_length:
@@ -58,9 +59,10 @@ def trim_audio(audio, seconds, fs):
 
 
 def compute_mfcc(audio, sr):
-    return librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=20)
+    return librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=100)
 
 # Navigate to the generated audio folder and extract the files
+print("curdir:", os.curdir)
 folder_path_generated = ".\\.\\Datasets\\WaveFake_GeneratedAudio\\generated_audio\\common_voices_prompts_from_conformer_fastspeech2_pwg_ljspeech"
 file_list = os.listdir(folder_path_generated)
 generated_audio_files = [file for file in file_list if file.endswith(".wav") or file.endswith(".mp3")]
@@ -70,86 +72,96 @@ folder_path_real = ".\\.\\Datasets\\LJSpeech_RealAudio\\LJSpeech-1.1\\wavs"
 file_list = os.listdir(folder_path_real)
 real_audio_files = [file for file in file_list if file.endswith(".wav") or file.endswith(".mp3")]
 
-generated_samples_20 = generated_audio_files[:20]
-real_samples_20 = real_audio_files[:20]
 
-print("Generated Audio Files: ", len(generated_samples_20))  
-print("Real Audio Files: ", len(real_samples_20))
+# First half is generated, second is real
+sampleNamesGenerated = generated_audio_files[:NUM_VALUES]
+sampleNamesReal = real_audio_files[:NUM_VALUES]
 
-FFTs_generated = []
-FFTs_real = []
-generated_audio_files = []
-real_audio_files = []
+#Generate labels for the audio files
+labels = [0] * len(sampleNamesGenerated) + [1] * len(sampleNamesReal)
+
+# Initialize the arrays to store the bispectrum, bicoherence and MFCCs
+bispectrum = []
+bicoherence = []
+mfcc = []
+ffts = []
+audio_files = []
 
 # Convert generated samples to mono
-for file in generated_samples_20:
+for file in sampleNamesGenerated:
     file_path = os.path.join(folder_path_generated, file)
     audio, fs = sf.read(file_path)
     if audio.ndim > 1:
         audio = audio.mean(axis=1)  # Take the average of the channels to convert to mono
-    audio = trim_audio(audio, 5, fs)
-    FFTs_generated.append(abs(fft.fft(audio)))
-    generated_audio_files.append(audio)
+    audio = trim_audio(audio, 3.5, fs)
+    ffts.append(abs(fft.fft(audio)))
+    audio_files.append(audio)
 
 # Convert real samples to mono
-for file in real_samples_20:
+for file in sampleNamesReal:
     file_path = os.path.join(folder_path_real, file)
     audio, fs = sf.read(file_path)
     if audio.ndim > 1:
         audio = audio.mean(axis=1)  # Take the average of the channels to convert to mono
-    audio = trim_audio(audio, 5, fs)
-    FFTs_real.append(abs(fft.fft(audio)))
-    real_audio_files.append(audio)
-    
+    audio = trim_audio(audio, 3.5, fs)
+    ffts.append(abs(fft.fft(audio)))
+    audio_files.append(audio)
+
 print("Sampling frequency: " + str(fs) + "Hz")
     
 # Compute bispectrum for all the samples, each bispectrum variable contains N number of samples, each sample is represented as an
 # array of the bispectrum values
-bispectrum_generated = compute_bispectrum(generated_audio_files)
-bispectrum_real = compute_bispectrum(real_audio_files)
+#bispectrum = compute_bispectrum(audio_files)
 
 # Calculate amplitude spectrum for the signals
-amplitude_spectrum_generated = calculate_amplitude_spectrum(generated_audio_files)
-amplitude_spectrum_real = calculate_amplitude_spectrum(real_audio_files)
+#amplitude_spectrum = calculate_amplitude_spectrum(audio_files)
 
 # Compute the bicoherence of the generated samples
-bicoh_generated = calculate_bicoherence(bispectrum_generated, amplitude_spectrum_generated)
+#bicoherence = calculate_bicoherence(bispectrum, amplitude_spectrum)
 
-# Compute the bicoherence of the real samples2
-bicoh_real = calculate_bicoherence(bispectrum_real, amplitude_spectrum_generated)
+#print("bicoh shape:", bicoherence[0].shape)
 
-# Print the length of the bicoherence list for generated samples
-print("Bicoherence generated size:" + str(len(bicoh_generated)))
-print("Bicoherence generated shape:" + str(bicoh_generated[0].shape))
+# Compute MFCCs for the samples
+mfcc = [ compute_mfcc(audio, fs) for audio in audio_files]
 
-# Print the length of the bicoherence list for real samples
-print("Bicoherence real size:" + str(len(bicoh_real)))
-print("Bicoherence real shape:" + str(bicoh_real[0].shape))
+# Reshape the MFCCs to a 2D array
+print(len(mfcc))
+print(len(mfcc[0]))
+print(len(mfcc[0][0]))
 
-# Compute MFCCs for generated samples
-mfcc_generated = [compute_mfcc(audio, fs) for audio in generated_audio_files]
-
-# Compute MFCCs for real samples
-mfcc_real = [compute_mfcc(audio, fs) for audio in real_audio_files]
-
-print("MFCC generated size:" + str(len(mfcc_generated)))
-print("MFCC real size:" + str(len(mfcc_real)))
-print("MFCC generated shape:" + str(mfcc_generated[0][0].shape))
-print("MFCC real shape:" + str(mfcc_real[0].shape))
+mfcc = np.array(mfcc)
+mfcc = mfcc.reshape(NUM_VALUES*2, 15100)
 
 # Convert complex numbers to real numbers by taking the absolute value
-bicoherence_abs_generated = np.abs(bicoh_generated)
-bicoherence_abs_real = np.abs(bicoh_real)
+#bicoherence_abs = np.abs(bicoherence)
 
-print("Bicoherence shape:", end="")
-print(bicoherence_abs_generated.shape)
-print(bicoherence_abs_generated)
+# Concatenate the bicoherence and MFCC arrays
+print("mfcc_generated shape:", len(mfcc), len(mfcc[0]))
 
-# ************************************Reduce dimension of data***********************************************
 
-bicoherence_abs_generated = StandardScaler().fit_transform(bicoherence_abs_generated)
-pca  = PCA(n_components=5000, whiten=True)
-X_pca = pca.fit_transform(bicoherence_abs_generated)
+# Perform delta cepstral and delta^2 analysis on the mfcc_bicoherence array
+delta_mfcc_bicoherence = librosa.feature.delta(mfcc)
+delta2_mfcc_bicoherence = librosa.feature.delta(mfcc, order=2)
 
-print("Original:" + str(bicoherence_abs_generated[0].shape))
-print("PCA:" + str(X_pca.shape))
+print("delta_mfcc_bicoherence shape:", len(delta_mfcc_bicoherence))
+print("delta_mfcc_bicoherence shape:", len(delta_mfcc_bicoherence[0]))
+print("delta2_mfcc_bicoherence shape:", len(delta2_mfcc_bicoherence))
+print("delta2_mfcc_bicoherence shape:", len(delta2_mfcc_bicoherence[0]))
+
+# Concatenate the bicoherence, MFCC, delta cepstral, delta^2 cepstral arrays to create a complete feature set
+#This is the most ideal one
+#feature_set = np.concatenate((mfcc, bicoherence_abs,delta_mfcc_bicoherence, delta2_mfcc_bicoherence), axis=1)
+#This is shorter and only includes features
+feature_set = np.concatenate((mfcc,delta_mfcc_bicoherence, delta2_mfcc_bicoherence), axis=1)
+
+
+print("feature_set shape:", len(feature_set), len(feature_set[0]))
+
+# Save the feature set and labels to a file
+np.save("feature_set.npy", feature_set)
+np.save("labels.npy", labels)
+
+random_array = np.random.randint(-1000, 1001, size=(40, 100000))
+
+np.save("random_array.npy", random_array)
+np.save("test.npy", random_array)
