d5e8481051bc0772ea6465f4d0fd3be28031d4fb
Maxim Lebedinsky <lebedinsky.maxim@gmail.com>
2024-06-18 10:47:07 +0300

    Merged code from 2 files into 1. Prevent code duplication.

--- Video preprocessing/preprocessing_real_data.py
+++ Video preprocessing/preprocessing_real_data.py
@@ -1,32 +1,32 @@
-import os
-import numpy as np
-from utils import extract_video_frame, img_to_video, get_video_paths, create_folder
+from pathlib import Path
+from utils import extract_video_frame, get_video_paths, create_folder
+from data import folder_path_to_datasets
 
+# Main function
 def main():
-    folder_path = "D:\\Dataset for Capstone Project\\dataset\\Celeb-DF-v2\\Celeb-real" 
-    video_paths = get_video_paths(folder_path)
+    # Path to folder which contains data
+    folder_path = folder_path_to_datasets['celeb_DF_real']
 
-    save_folder = "Video preprocessing\\Processed_data\\Celeb-DF-v2\\Celeb-real"
-    create_folder(save_folder)
+    # Path to folder in which processed data will be saved
+    save_folder = "Video preprocessing/Processed_data_real/Celeb-DF-v2/Celeb-real"
+   
+    types = ["train", "test", "val"]
+    for type in types:
+        # Get paths of all videos
+        video_paths = get_video_paths(folder_path + '/' + type)
+        # If such folder does not exist, create it
+        save_folder = save_folder  + '/' + type
+        create_folder(save_folder)
 
-    # Iterate each video, extract the frames which include faces 
-    for video_path in video_paths:
-        video_name = os.path.basename(video_path)
-        video_name = save_folder + '/' + video_name
-        extract_video_frame(video_path=video_path, video_name=video_name)
-
-    '''for np_array in os.listdir(extracted_frames):
-        images_array = np.load(extracted_frames + '/' + np_array)
-        #frame_count = 0
-        video_num = 0
-        for image_array in images_array:
-            face_detection(image_array, extracted_face_frames + '/' + np_array[-4], video_name=extracted_frames + '/' + (video_paths[video_num])[:-4])
-            #frame_count +=1
-            video_num+=1
-    '''
-    
-    '''processed_face_video_folder = 'Video preprocessing/processed_face_videos_real'
-    # Convert face frames back to video
-    img_to_video(output_filename=processed_face_video_folder, fps=30, height=250, width=250, channels=3, images_folder=extracted_frames)'''
+        i = 1
+        # Iterate each video, extract the frames which include faces 
+        for video_path in video_paths:
+            print(f"Video {i}/{len(video_paths)}")
+            # Get video name
+            video_name = Path(video_path).stem
+            video_name = save_folder + '/' + video_name
+            # Preprocesse the video
+            extract_video_frame(video_path=video_path, video_name=video_name)
+            i+=1
 
 main()
