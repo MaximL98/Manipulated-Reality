7dad4bedfef2b1290b92d6fe2ee22d6f4baee051
Maxim Lebedinsky <lebedinsky.maxim@gmail.com>
2024-06-27 15:46:43 +0300

    Process only 10% of files, for smaller model

--- video_analysis/video_preprocessing/preprocessing_data.py
+++ video_analysis/video_preprocessing/preprocessing_data.py
@@ -11,13 +11,17 @@ def main():
     # List of paths to folders into which after processing the npy files will be saved
     save_folders = [paths_to_folders_after_processing ['celeb_DF_real'],
                     paths_to_folders_after_processing['celeb_DF_fake']]
+    # Get total number of files in a folder
+    num_files = len(folder_paths)
+    # For smaller model process only 10-20% of the data, for full model 100%
+    num_to_process = int(num_files*0.1)
     # For loop, process both real and fake data
-    for index in range(len(folder_paths)):
+    for index in range(num_to_process):
         # Path to folder which contains data
         folder_path = folder_paths[index]
         # Path to folder in which processed data will be saved
         save_folder = save_folders[index]
-        # List of availble types in which the data was split
+        # List of available types in which the data was split
         types = ["train", "test", "val"]
         for type in types:
             # Get paths of all videos
@@ -33,7 +37,7 @@ def main():
                 # Get video name
                 video_name = Path(video_path).stem
                 video_name = tmp_save_folder + '/' + video_name
-                # Preprocesse the video
+                # Preprocess the video
                 extract_video_frame(video_path=video_path, video_name=video_name)
                 i+=1
 
