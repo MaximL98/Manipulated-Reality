Code refactoring, and normalization step fix small errors.

video_analysis/video_preprocessing/main.py
@@ -1,12 +1,13 @@
-import numpy as np
+# Import time to calculate how long normalization step takes
+import time
+
 from pathlib import Path
 
-from utils import label_data, normalize_frames, create_folder, create_normalization_folders
+from utils import label_data, normalize_frames, create_normalization_folders
 from data import processed_data_folder_paths
 
 # Main function to split and normalize the data
 def main():
-    
     # Path to fake and real data, after frame extraction and face recognition
     fake_video_folder_train = processed_data_folder_paths['fake_video_folder_train']
     fake_video_folder_test = processed_data_folder_paths['fake_video_folder_test']
@@ -19,37 +20,25 @@ def main():
     train_df, test_df, val_df = label_data(real_train_folder=real_video_folder_train, real_test_folder=real_video_folder_test, real_val_folder=real_video_folder_val, 
               fake_train_folder=fake_video_folder_train, fake_test_folder=fake_video_folder_test, fake_val_folder=fake_video_folder_val)
 
-    # Three arrays to save to the frames after they been normalized
-    normalized_frames_train = []
-    normalized_frames_test = []
-    normalized_frames_val = []
-
+    # Create normalization folders, to which the data will be saved
     train_folder, test_folder, val_folder = create_normalization_folders()
-
-    # Three for loop to normalize train, test and, validation datasets
-    for video_path in train_df['video_path']:
-        print(f"Starting to normalize {video_path} ...")
-        # Get video name
-        video_name = Path(video_path).stem
-        video_name = train_folder + '/' + video_name
-        normalized_frames_train = normalize_frames(video_path)
-        np.save(f"{video_name}.npy", normalized_frames_train)
-    
-    for video_path in test_df['video_path']:
-        print(f"Starting to normalize {video_path} ...")
-        # Get video name
-        video_name = Path(video_path).stem
-        video_name = test_folder + '/' + video_name
-        normalized_frames_test = normalize_frames(video_path)
-        np.save(f"{test_folder}.npy", normalized_frames_test)
-
-    for video_path in val_df['video_path']:
-        print(f"Starting to normalize {video_path} ...")
-        # Get video name
-        video_name = Path(video_path).stem
-        video_name = val_folder + '/' + video_name
-        normalized_frames_val = normalize_frames(video_path)
-        np.save(f"{val_folder}.npy", normalized_frames_val)
+    # All DataFrames and folders
+    dfs = [train_df, test_df, val_df]
+    folders = [train_folder, test_folder, val_folder]
+    # Loop through each DataFrame and save to a specific folder
+    for index in range(3):
+        # For-loop to normalize train, test and, validation datasets
+        for video_path in dfs[index]['video_path']:
+            start_time = time.time()
+            print(f"Starting to normalize {video_path} ...")
+            # Get video name
+            video_name = Path(video_path).stem
+            video_name = folders[index] + '/' + video_name
+            #normalized_frames_train = normalize_frames(video_path)
+            #np.save(f"{video_name}.npy", normalized_frames_train)
+            normalize_frames(video_path, video_name)
+            print("--- %s seconds ---" % (time.time() - start_time))
+            
 
 if __name__ == '__main__':
     main()

video_analysis/video_preprocessing/utils.py
@@ -240,7 +240,12 @@ def label_data(real_train_folder, real_test_folder, real_val_folder,
 
 
 # Function to normalize frames
-def normalize_frames(video_path):
+def normalize_frames(video_path, video_name):
+    print(video_name)
+    # Check if file was all ready processed
+    if os.path.exists(video_name + '_normalized.npy'):
+        print(f"This file ({video_name}) already normalized!")
+        return
     # Load frames from video path
     frames = np.load(video_path)
     # Custom transform function
@@ -267,8 +272,8 @@ def normalize_frames(video_path):
         frame_normalized = frame_normalized.transpose(1, 2, 0)
         # Append normalized frame into array
         frames_normalized.append(frame_normalized)
-    # Return normalized frames array
-    return frames_normalized
+
+    np.save(f"{video_name}_normalized.npy", frames_normalized)
 
 
 # Function that creates folders into which data will be saved after normalization
@@ -278,7 +283,7 @@ def create_normalization_folders():
     train_folder = paths_to_folders_after_normalization['train_folder']
     test_folder = paths_to_folders_after_normalization['test_folder']
     val_folder = paths_to_folders_after_normalization['val_folder']
-    # Create folder to save into the normilized data
+    # Create folder to save into the normalized data
     create_folder(train_folder)
     create_folder(test_folder)
     create_folder(val_folder)