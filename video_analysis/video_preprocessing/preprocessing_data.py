db9a84921f29cad4d679b2df540b480de34a9c07
Maxim Lebedinsky <lebedinsky.maxim@gmail.com>
2024-06-28 16:17:18 +0300

    Small fix

--- video_analysis/video_preprocessing/preprocessing_data.py
+++ video_analysis/video_preprocessing/preprocessing_data.py
@@ -11,12 +11,8 @@ def main():
     # List of paths to folders into which after processing the npy files will be saved
     save_folders = [paths_to_folders_after_processing ['celeb_DF_real'],
                     paths_to_folders_after_processing['celeb_DF_fake']]
-    # Get total number of files in a folder
-    num_files = len(folder_paths)
-    # For smaller model process only 10-20% of the data, for full model 100%
-    num_to_process = int(num_files*0.1)
     # For loop, process both real and fake data
-    for index in range(num_to_process):
+    for index in range(len(folder_paths)):
         # Path to folder which contains data
         folder_path = folder_paths[index]
         # Path to folder in which processed data will be saved
@@ -30,10 +26,17 @@ def main():
             tmp_save_folder = save_folder  + '/' + type
             create_folder(tmp_save_folder)
             # Initialize index to follow how many videos finished processing
-            i = 1
-            # Iterate each video, extract the frames which include faces 
+            i = 0
+            # Get total number of files in a folder
+            num_files = len(video_paths)
+            # For smaller model process only 10-20% of the data, for full model 100%
+            num_to_process = int(num_files*0.1)
+            # Iterate each video, extract the frames which include faces
             for video_path in video_paths:
-                print(f"Video {i}/{len(video_paths)}")
+                if i == num_to_process:
+                    print(f"Reached set limit of {num_to_process} out of {num_files}")
+                    break;
+                print(f"Video {i + 1}/{num_files}")
                 # Get video name
                 video_name = Path(video_path).stem
                 video_name = tmp_save_folder + '/' + video_name
