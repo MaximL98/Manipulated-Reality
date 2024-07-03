5f96da520f7099e16f6db54e21d6632b558670a2
Maxim Lebedinsky <lebedinsky.maxim@gmail.com>
2024-07-03 14:09:47 +0300

    Fixed some problems

--- video_analysis/video_model/train.py
+++ video_analysis/video_model/train.py
@@ -1,40 +1,34 @@
 
 import keras
-import numpy as npc
+import numpy as np
 
-from model import model
+from model import get_model
 from utils import batch_generator, load_data_from_csv
 from data import paths_to_csv
 
 
-'''batch_size = 4
-train_generator = batch_generator(paths_to_csv['train_df'], batch_size)
-test_generator = batch_generator(paths_to_csv['test_df'], batch_size, is_training=False)
-val_generator = batch_generator(paths_to_csv['val_df'], batch_size, is_training=False)'''
+batch_size = 8
+train_generator, train_labels = batch_generator(paths_to_csv['train_df'], batch_size)
+test_generator, test_labels = batch_generator(paths_to_csv['test_df'], batch_size, is_training=False)
+val_generator, val_labels = batch_generator(paths_to_csv['val_df'], batch_size, is_training=False)
 
 
-train_data, train_labels = load_data_from_csv(paths_to_csv['train_df'])
-test_data, test_labels = load_data_from_csv(paths_to_csv['test_df'])
-val_data, val_labels = load_data_from_csv(paths_to_csv['val_df'])
-
-model_ = model()
-
-#for batch_videos, batch_labels,  in train_generator:
-## Get frames and their labels
-#frames, label = next(iter(train_generator))
-    ## Build the model
-model_.build(input_shape=(None, 225, 224, 224, 3))
+model = get_model()
+## Build the model
+model.build(input_shape=(None, 225, 224, 224, 3))
 ## Visualize the model
-keras.utils.plot_model(model_, expand_nested=True, dpi=60, show_shapes=True)
+#keras.utils.plot_model(model, expand_nested=True, dpi=60, show_shapes=True)
 ## Train the model
 ## Binary classification problems with two classes, the most appropriate loss function is Binary Cross entropy
-model_.compile(loss = keras.losses.BinaryCrossentropy(from_logits=True), 
-              optimizer = keras.optimizers.Adam(learning_rate = 0.0001), 
+model.compile(loss = keras.losses.BinaryCrossentropy(from_logits=True), 
+              optimizer = keras.optimizers.Adam(learning_rate = 0.0001),
               metrics = ['accuracy'])
 
+print("Starting to train model...")
 ## Better to start with less epochs 20-30
-history = model_.fit(x = train_data,
-                    epochs = 50, 
-                    validation_data = val_data)
-## need to return history
-model_.evaluate(test_data, return_dict=True)
+#history = model.fit(x = train_generator,epochs = 20, validation_data = val_generator)
+
+model.fit(x=train_generator, y=train_labels, epochs=10)
+# Evaluate on test data
+test_results = model.evaluate(test_generator, return_dict=True)
+print("Test results:", test_results)
