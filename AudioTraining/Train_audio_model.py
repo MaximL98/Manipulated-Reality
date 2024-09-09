43c4a3bc940cc3f9d313106e065ee8fdf2ae567e
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2024-09-09 09:27:50 +0300

    Pull origin master

--- AudioTraining/Train_audio_model.py
+++ AudioTraining/Train_audio_model.py
@@ -3,72 +3,33 @@ import numpy as np
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, RobustScaler
-
 import tensorflow as tf # tested with 1.14.0
-
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
 from sklearn.metrics import classification_report # tested with 0.21.2l
 from tensorflow import keras
 from keras import models
 from keras import layers
-
 # Supress deprecation warnings
 import logging
 logging.getLogger('tensorflow').disabled = True
-  
 os.environ['PYTHONIOENCODING'] = 'UTF-8'
 
-class_names = ["fake", "real"]
 
-# #train_data = np.load('feature_set.npy')
-# train_data = np.load("Audio_numpy_files\\0(REAL)_to_1(REAL)_0(FAKE)_to_1(FAKE)_samples_DeepVoice_dataset_feature_set.npy")
-
-# # labels = np.load('labels.npy')
-# labels = np.load("Audio_numpy_files\\0(REAL)_to_1(REAL)_0(FAKE)_to_1(FAKE)_samples_DeepVoice_dataset_labels_set.npy")
-# labels = labels.astype('int32')
-
+# Numpy pre-processed audio files are listed here
 array_of_feature_set_names = ["All_data_1000ms_22050hz\\0-8_(REAL)_0-56_(FAKE)_1000ms_for_sample_22050hz_frequency_DeepVoice_dataset_feature_set.npy",
                               "All_data_1000ms_22050hz\\0-31779_samples_1000ms_for_sample_22050hz_frequency_InTheWild_dataset_feature_set.npy",
                               "All_data_1000ms_22050hz\\0-30043_(REAL)_0-0_(FAKE)_1000ms_for_sample_22050hz_frequency_FluentSpeechCorpus_feature_set.npy",
-                              "All_data_1000ms_22050hz\\0-3106_(REAL)_0-16283_(FAKE)_1000ms_for_sample_22050hz_frequency_WaveFake_dataset_feature_set.npy"
-]
+                              "All_data_1000ms_22050hz\\0-3106_(REAL)_0-16283_(FAKE)_1000ms_for_sample_22050hz_frequency_WaveFake_dataset_feature_set.npy"]
 
 array_of_labels_set_names = [ "All_data_1000ms_22050hz\\0-8_(REAL)_0-56_(FAKE)_1000ms_for_sample_22050hz_frequency_DeepVoice_dataset_labels_set.npy",
                               "All_data_1000ms_22050hz\\0-31779_samples_1000ms_for_sample_22050hz_frequency_InTheWild_dataset_labels_set.npy",
                               "All_data_1000ms_22050hz\\0-30043_(REAL)_0-0_(FAKE)_1000ms_for_sample_22050hz_frequency_FluentSpeechCorpus_labels_set.npy",
-                              "All_data_1000ms_22050hz\\0-3106_(REAL)_0-16283_(FAKE)_1000ms_for_sample_22050hz_frequency_WaveFake_dataset_labels_set.npy"
-                              
-]
+                              "All_data_1000ms_22050hz\\0-3106_(REAL)_0-16283_(FAKE)_1000ms_for_sample_22050hz_frequency_WaveFake_dataset_labels_set.npy"]
 
-# array_of_feature_set_names = [ "5_10_percent_1000ms_22050hz\\0-4_(REAL)_0-4_(FAKE)_1000ms_for_sample_DeepVoice_dataset_feature_set.npy",
-#                               "5_10_percent_1000ms_22050hz\\0-1500_(REAL)_0-1500_(FAKE)_1000ms_for_sample_WaveFake_dataset_feature_set.npy",
-#                               "5_10_percent_1000ms_22050hz\\0-1500_(REAL)_0-0_(FAKE)_1000ms_for_sample_FluentSpeechCorpus_dataset_feature_set.npy",
-#                               "5_10_percent_1000ms_22050hz\\0-1500_samples_1000ms_for_sample_InTheWild_dataset_feature_set.npy"
-# ]
-
-# array_of_labels_set_names = ["5_10_percent_1000ms_22050hz\\0-4_(REAL)_0-4_(FAKE)_1000ms_for_sample_DeepVoice_dataset_labels_set.npy",
-#                               "5_10_percent_1000ms_22050hz\\0-1500_(REAL)_0-1500_(FAKE)_1000ms_for_sample_WaveFake_dataset_labels_set.npy",
-#                               "5_10_percent_1000ms_22050hz\\0-1500_(REAL)_0-0_(FAKE)_1000ms_for_sample_FluentSpeechCorpus_dataset_labels_set.npy",
-#                               "5_10_percent_1000ms_22050hz\\0-1500_samples_1000ms_for_sample_InTheWild_dataset_labels_set.npy"
-# ]
-
-# array_of_feature_set_names = [ "2_5_percent_1000ms\\0-1_(REAL)_0-1_(FAKE)_samples_DeepVoice_dataset_feature_set.npy",
-#                               "2_5_percent_1000ms\\0-1000_(REAL)_0-0_(FAKE)_samples_FluentSpeechCorpus_dataset_feature_set.npy",
-#                               "2_5_percent_1000ms\\0-500_(REAL)_0-367_(FAKE)_samples_InTheWild_dataset_feature_set.npy"
-# ]
-
-# array_of_labels_set_names = ["2_5_percent_1000ms\\0-1_(REAL)_0-1_(FAKE)_samples_DeepVoice_dataset_labels_set.npy",
-#                               "2_5_percent_1000ms\\0-1000_(REAL)_0-0_(FAKE)_samples_FluentSpeechCorpus_dataset_labels_set.npy",
-#                               "2_5_percent_1000ms\\0-500_(REAL)_0-367_(FAKE)_samples_InTheWild_dataset_labels_set.npy"
-# ]
-
-
-
-
-
+# Class names
 class_names = ["fake", "real"]
 
+# Create a list of all the features
 train_data = []
 for dataset_features_name in array_of_feature_set_names:
     destination_dataset = 'Audio_numpy_files\\' + dataset_features_name
@@ -77,6 +38,7 @@ for dataset_features_name in array_of_feature_set_names:
 
 train_data = np.concatenate(train_data, axis=0)
 
+# Create a list of all the labels
 labels = []
 for dataset_labels_name in array_of_labels_set_names:
     destination_dataset = 'Audio_numpy_files\\' + dataset_labels_name
@@ -85,57 +47,23 @@ for dataset_labels_name in array_of_labels_set_names:
 
 labels = np.concatenate(labels, axis=0)
 labels = labels.astype('int32')
-
-print("***************Before scaling****************")
-for i in range(20):
-    print(train_data[-i-10], " label: " , labels[-i-10])
     
 
-#scaler = MinMaxScaler((-1, 1))
+# Scale the audio data using RobustScaler
 scaler = RobustScaler()
 train_data = scaler.fit_transform(train_data)
 
+# Save the scaler
+scaler_filename = "scaler.save"
 import pickle
-
-with open('scaler.pkl', 'wb') as file:
+with open(scaler_filename, 'wb') as file:
     pickle.dump(scaler, file)
 
-
-
-print("***************After scaling****************")
-for i in range(20):
-    print(train_data[-i-10], " label: " , labels[-i-10])
-
-####################################################### TEST ####################################################################
-test_dataset_one_file = np.load("Audio_numpy_files\\All_data_1000ms_22050hz\\0-1_(REAL)_0-1_(FAKE)_1000ms_for_sample_real_fake_one_feature_set.npy")
-test_dataset_one_file_labels = np.load("Audio_numpy_files\\All_data_1000ms_22050hz\\0-1_(REAL)_0-1_(FAKE)_1000ms_for_sample_real_fake_one_labels_set.npy")
-test_dataset_one_file_labels = test_dataset_one_file_labels.astype('int32')
-test_dataset_one_file = scaler.fit_transform(test_dataset_one_file)
-####################################################### TEST ####################################################################
-
-
-print(labels.shape)
-print(train_data.dtype)
-print(labels.dtype)
-print(train_data[0][0].dtype)
-
-
-
+# Split the data into training, validation and testing sets
 x_train, x_test, y_train, y_test = train_test_split(train_data, labels, test_size=0.25, random_state=123)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=123)
 
-print(x_train.shape)
-
-print(len(labels))
-
-print("x_train shape:", x_train.shape)
-print("x_train.shape[0]: ",x_train.shape[0])
-print("x_train.shape[1]: ",x_train.shape[1])
-
-print("x_val shape:", x_val.shape)
-
-
-# Input shape is a tuple of 600 float32 and 1 dimension
+# Create the model
 inputs = layers.Input(shape=(x_train.shape[1], 1))
 CNNmodel = models.Sequential([
     inputs,
@@ -173,18 +101,6 @@ CNNmodel = models.Sequential([
     # layers.Dense(32, activation='relu'),
     # layers.Dense(24, activation='softmax')
 ])
-# CNNmodel.add()
-# CNNmodel.add(MaxPooling1D(pool_size=2))
-# CNNmodel.add(Dropout(0.2))
-# CNNmodel.add(Conv1D(64, kernel_size=3, activation='relu'))
-# CNNmodel.add(MaxPooling1D())
-# CNNmodel.add(Dropout(0.2))
-# CNNmodel.add(Conv1D(64, kernel_size=3, activation='relu'))
-# CNNmodel.add(Flatten())
-# CNNmodel.add(Dense(64, activation='relu'))
-# CNNmodel.add(Dropout(0.2))
-# CNNmodel.add(Dense(32, activation='relu'))
-# CNNmodel.add(Dense(24, activation='softmax'))
 
 # Display a summary of the models structure
 CNNmodel.summary()
@@ -192,24 +108,14 @@ CNNmodel.summary()
 # Build the model
 CNNmodel.compile(optimizer='adam', loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy', 'precision', 'recall' ])
 
-
+# Train the model
 history = CNNmodel.fit(x_train,y_train, batch_size=256, epochs=3, validation_data= (x_val, y_val))
 
+# Save the model
 CNNmodel.save("2_Audio_model.keras")
 
-
-print(x_train.shape)
-print(x_train[0].shape)
-print(x_train[1].shape)
-print(x_train[0][0].shape)
-
-
-print(y_train)
-
-
+# Evaluate the model
 predict_x=CNNmodel.predict(x_test)
-print(predict_x[0:20])
-
 predicted_classes = (predict_x >= 0.5).astype("int32")
 counter = 0
 
@@ -221,7 +127,6 @@ for i in range(len(predict_x)):
 print("Size: ", len(predict_x), "Counter: ", counter)
 
 print(classification_report(y_test, predicted_classes, target_names=class_names))
-
 
 from sklearn.metrics import confusion_matrix
 
